830dc1693cabc40669c68ef2a766290c808aeba2
spin877 <66121170+spin877@users.noreply.github.com>
2023-11-11 00:02:48 +0100

    first

---
+++ Bruciatore_BLE.py
@@ -0,0 +1,56 @@
+class Bruciatore_BLE:
+    def __init__(self, byte_sequence):
+        self.byte_sequence = byte_sequence
+
+    def get_accensione(self):
+        return bool(self.byte_sequence[3] )
+  
+    def get_accensione_candelette(self):
+        if self.byte_sequence[5] == 0:            
+            return 'Spenta'
+        if self.byte_sequence[5] == 2:            
+            return 'Accesa'
+        if self.byte_sequence[5] == 3:            
+            return 'Stambay mode'
+        if self.byte_sequence[5] == 4:            
+            return 'Spegnimento'
+        
+    def get_potenza(self):
+        return self.byte_sequence[10] +1
+
+    def get_temperatura(self):
+        return self.byte_sequence[15] 
+
+    def get_temperatura_uscita_bruciatore(self):
+        return self.byte_sequence[13] # 
+
+    def get_temperatura_bruciatore(self):
+        return self.byte_sequence[19] # 
+
+    def get_temperatura_target(self):
+        return self.byte_sequence[9] # non è una temperatura sembra seguire la potenza
+    
+    def get_Volt_battery(self):    
+        return self.byte_sequence[11] / 10.0
+    
+    def get_Altitudine(self):
+        return self.byte_sequence[6]
+
+    def dump(self):
+
+        if len(self.byte_sequence)> 21:                                                  
+                print(f"Accensione: {'Acceso' if self.get_accensione() else 'Spento'}")
+                print(f"Potenza: {self.get_potenza()}")
+                print(f"Temperatura: {self.get_temperatura()}°C")
+                print(f"Temperatura interna Bruciatore: {self.get_temperatura_bruciatore()}°C")
+                print(f"Temperatura Uscita Bruciatore: {self.get_temperatura_uscita_bruciatore()}°C")
+                print(f"Temperatura Target: {self.get_temperatura_target()}°C")
+                print(f"Candeletta Modalita: {self.get_accensione_candelette()}")
+                print(f"Volt Batteria: {self.get_Volt_battery()} Volt")
+                print(f"Decimal representation: {[x for x in self.byte_sequence]}")
+                print(f"Altitudine: {self.get_Altitudine()}")
+                print(f"\r\ni comandi supportati sono cmd1-10\r\n")
+                print()
+        else:
+            print("il Bruciatore è spento")            
+
---
+++ Vevor_AirHeater_BLE.py
@@ -0,0 +1,104 @@
+import os
+from bluepy.btle import Peripheral, DefaultDelegate
+import threading
+import time
+
+from Bruciatore_BLE import Bruciatore_BLE  # Importa la classe Bruciatore_BLE dal modulo Bruciatore_ble
+
+
+# Creare una sottoclasse di DefaultDelegate per gestire le notifiche
+class MyDelegate(DefaultDelegate):
+    def handleNotification(self, cHandle, data):
+        # Questo metodo verrà chiamato quando una notifica è ricevuta
+        bruciatore = Bruciatore_BLE(list(data))
+        bruciatore.dump()
+
+
+# Indirizzo del dispositivo BLE a cui desideri connetterti
+target_address = "21:47:08:1a:b1:1e"
+
+# Servizio e caratteristica di interesse
+service_uuid = "0000ffe0-0000-1000-8000-00805f9b34fb"
+characteristic_uuid = "0000ffe1-0000-1000-8000-00805f9b34fb"
+
+# Definisci i comandi
+common_bytes = bytearray([0xaa, 0x55, 0x0c, 0x22])
+
+cmd1  = common_bytes + bytearray([0x01, 0x00, 0x00, 0x2f])
+
+#cmd2  = common_bytes + bytearray([0x04, 0xa0, 0x00, 0x3c])
+
+# cmd3  = common_bytes + bytearray([0x02, 0x02, 0x00, 0x32]) # lampeggia temeratura target per regolarla #36 gradi
+# cmd4  = common_bytes + bytearray([0x04, 0x24, 0x00, 0x56]) # lampeggia 8 gradi
+
+
+cmd8  = common_bytes + bytearray([0x03, 0x00, 0x00, 0x31]) # OFF
+cmd10 = common_bytes + bytearray([0x03, 0x01, 0x00, 0x32]) # ON
+
+cmd11 = common_bytes + bytearray([0x04, 0x01, 0x00, 0x33])  #1
+cmd12 = common_bytes + bytearray([0x04, 0x02, 0x00, 0x34])  #2
+cmd13 = common_bytes + bytearray([0x04, 0x03, 0x00, 0x35])  #3
+cmd14 = common_bytes + bytearray([0x04, 0x04, 0x00, 0x36])  #4
+cmd15 = common_bytes + bytearray([0x04, 0x05, 0x00, 0x37])  #5
+cmd16 = common_bytes + bytearray([0x04, 0x06, 0x00, 0x38])  #6
+cmd17 = common_bytes + bytearray([0x04, 0x07, 0x00, 0x39])  #7
+cmd18 = common_bytes + bytearray([0x04, 0x08, 0x00, 0x3a])  #8
+cmd19 = common_bytes + bytearray([0x04, 0x09, 0x00, 0x3b])  #9
+
+def auto_command_thread():
+    while True:
+        # Invia automaticamente il comando cmd1
+        response = characteristic.write(cmd1, withResponse=True)
+
+        data = characteristic.read()
+
+        time.sleep(1)  # Attendere 2 secondi prima di inviare il comando successivo
+        print("\r\ni comandi supportati sono cmd1-19\r\n")
+
+try:
+    # Connessione al dispositivo BLE
+    peripheral = Peripheral(target_address, "public")
+
+    # Trova il servizio desiderato
+    service = peripheral.getServiceByUUID(service_uuid)
+
+    if service is not None:
+        # Trova la caratteristica desiderata
+        characteristic = service.getCharacteristics(characteristic_uuid)[0]
+
+        if characteristic is not None:
+            peripheral.setDelegate(MyDelegate())
+            
+            # Creare e avviare il thread per l'invio automatico del comando
+            auto_thread = threading.Thread(target=auto_command_thread)
+            auto_thread.daemon = True  # Il thread si esce quando il programma principale termina
+            auto_thread.start()
+
+            while True:
+                command = input("Inserisci un comando (o 'exit' per uscire): ").strip()
+                if command.lower() in ['exit', 'close', 'quit', 'q']:
+                    break
+                elif command.startswith('cmd'):
+                    # Ottieni il numero del comando
+                    cmd_num = int(command[3:])
+                    if cmd_num >= 1 and cmd_num <= 21:
+                        # Ottieni il comando corrispondente
+                        cmd = eval(f'cmd{cmd_num}')
+                        # Invia il comando come richiesta
+                        response = characteristic.write(cmd, withResponse=True)
+
+                    else:
+                        print("Comando non valido. Inserisci un comando da 'cmd1' a 'cmd10'.")
+                else:
+                    print("Comando non valido. Inserisci un comando da 'cmd1' a 'cmd10' o 'exit'.")
+        else:
+            print(f"Caratteristica con UUID {characteristic_uuid} non trovata.")
+    else:
+        print(f"Servizio con UUID {service_uuid} non trovato.")
+
+    # Disconnettiti dal dispositivo BLE
+    peripheral.disconnect()
+
+except Exception as e:
+    print(f"Errore durante la connessione o la lettura dei dati: {str(e)}")
+
